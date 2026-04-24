d82c2186489f38adbab0a12beddc990d7b8494da
EllisJFrank <ellis@mcfranko.com>
2026-04-24 14:52:09 -0700

    NetworkState.chunksGenerated is now persistent between joins.

--- src/main/java/com/ethan/voxyworldgenv2/network/NetworkState.java
+++ src/main/java/com/ethan/voxyworldgenv2/network/NetworkState.java
@@ -2,6 +2,28 @@ package com.ethan.voxyworldgenv2.network;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import net.fabricmc.fabric.api.resource.v1.reloader.ResourceReloaderKeys.Client;
+import net.fabricmc.loader.api.FabricLoader;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.FriendlyByteBuf;
+
+
+import org.apache.logging.log4j.Level;
+
+import com.ethan.voxyworldgenv2.VoxyWorldGenV2;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.Files;
+import java.io.File;
+
+import java.io.BufferedOutputStream;
+
 public class NetworkState {
     private static boolean serverConnected = false;
     private static final AtomicLong chunksReceived = new AtomicLong(0);
@@ -14,9 +36,74 @@ public class NetworkState {
     private static long lastChunkCount = 0;
     private static long lastByteCount = 0;
 
+    public static void save() {
+        Path savePath = savePath();
+        VoxyWorldGenV2.LOGGER.info("trying to save network state to path: " + savePath);
+
+        File clientDirectory = new File(FabricLoader.getInstance().getGameDir().resolve(".voxyworldgen").toUri());
+        VoxyWorldGenV2.LOGGER.info("does client directory exist: " + clientDirectory.exists());
+        VoxyWorldGenV2.LOGGER.info("client directory: " + clientDirectory.toString());
+        if (!clientDirectory.exists()) {
+            VoxyWorldGenV2.LOGGER.info("Voxy world gen client directory does not exist, attempting to create it: " + clientDirectory);
+            try {
+                if (!clientDirectory.mkdir()) {
+                    VoxyWorldGenV2.LOGGER.error("could not create client directory");
+                }
+            } catch (Exception e) {
+                VoxyWorldGenV2.LOGGER.error("could not create client directory bad", e);
+            }
+        }
+
+        File stateFile = new File(savePath.toUri());
+        if (!stateFile.exists()) {
+            try {
+                if (!stateFile.createNewFile()) {
+                    VoxyWorldGenV2.LOGGER.error("could not create network state file");
+                }
+            } catch (Exception e) {
+                VoxyWorldGenV2.LOGGER.error("could not create network state file", e);
+            }
+        }
+
+        try (DataOutputStream out = new DataOutputStream(new BufferedOutputStream(Files.newOutputStream(savePath, StandardOpenOption.CREATE)))) {
+            out.writeLong(chunksReceived.get());
+        } catch (Exception e) {
+            VoxyWorldGenV2.LOGGER.error("failed to save network state", e);
+        }
+    }
+
+    public static void load() {
+        Path savePath = savePath();
+        VoxyWorldGenV2.LOGGER.info("trying to load network state from path: " + savePath);
+
+        try (DataInputStream in = new DataInputStream(new BufferedInputStream(Files.newInputStream(savePath)))) {
+            Long toSetChunks = in.readLong();
+            chunksReceived.set(toSetChunks);
+            Long hello = chunksReceived.get();
+            VoxyWorldGenV2.LOGGER.info("tried to set Chunks recieved to: " + toSetChunks.toString() + " it is now set to: " + hello.toString());
+        } catch (Exception e) {
+            VoxyWorldGenV2.LOGGER.error("failed to load network state", e);
+        }
+    }
+
+    private static Path savePath() {
+        String worldName = null;
+        if (Minecraft.getInstance().getSingleplayerServer() != null) {
+            worldName = Minecraft.getInstance().getSingleplayerServer().name();
+        } else {
+            worldName = Minecraft.getInstance().getCurrentServer().ip;
+        }
+
+        return FabricLoader.getInstance().getGameDir().resolve(".voxyworldgen/" + worldName + ".bin");
+    }
+
+
     public static void setServerConnected(boolean connected) {
+        VoxyWorldGenV2.LOGGER.info("setting server connected state to: " + connected);
+
         serverConnected = connected;
         if (!connected) {
+            save();
             chunksReceived.set(0);
             bytesReceived.set(0);
             receiveRate = 0;
@@ -24,6 +111,8 @@ public class NetworkState {
             lastUpdateTime = 0;
             lastChunkCount = 0;
             lastByteCount = 0;
+        } else if (connected) {
+            load();
         }
     }
 
